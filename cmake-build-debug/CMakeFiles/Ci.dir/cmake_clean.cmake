a9945db7580d0c872cb16bb594764ef2fb1eae01
temnoed <dc9257351633@gmail.com>
2017-01-12 23:45:25 +0300

    си начало. дз 1.

---
+++ cmake-build-debug/CMakeFiles/Ci.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ci.dir/main.c.obj"
+  "Ci.pdb"
+  "Ci.exe"
+  "Ci.exe.manifest"
+  "libCi.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Ci.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int h;
+    int m;
+    int bmi = 0;
+
+    printf("Индекс массы тела =", bmi);
+    return 0;
+
+
+}

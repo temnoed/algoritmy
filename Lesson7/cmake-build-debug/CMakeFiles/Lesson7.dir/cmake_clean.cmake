a00a3d523c9877a2cfde04366ae6743c518e32fe
temnoed <dc9257351633@gmail.com>
2017-02-01 17:17:22 +0300

    Алгоритмы. Урок 7. Рекурсия.  ДЗ.

---
+++ Lesson7/cmake-build-debug/CMakeFiles/Lesson7.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lesson7.dir/main.c.obj"
+  "Lesson7.pdb"
+  "Lesson7.exe"
+  "Lesson7.exe.manifest"
+  "libLesson7.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lesson7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

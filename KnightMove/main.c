10c5e4f178d56052ece215b67c2b827cdd447315
temnoed <dc9257351633@gmail.com>
2017-02-02 04:26:45 +0300

    готово.

--- KnightMove/main.c
+++ KnightMove/main.c
@@ -1,20 +1,29 @@
+// Чистяков Дмитрий. Алгоритмы. ДЗ7.
 // Рекурсия с возвратом. Обход доски конём.
 // Готовое решение с опечатками оформлено и подправлено из
 // "Язык Си: шаг за шагом (автоматизация и применение)" Романов Е.Л.
-// А также "Алгоритмы и структуры данных" Вирт Н.
+// Изначально у автора в функции было 5 операторов return.
 #include <stdio.h>
 #include <stdlib.h>
+#include <afxres.h>
+#include <sys/time.h>
+// подключаем время
+struct timeval tv1, tv2, dtv;
+struct timezone tz;
+void time_start();
+long time_stop();
 
 // Используем глобальные переменные как в оригинале решения
 // размер доски 5*5 для ускорения:
 #define deskX 5
 #define deskY 5
 int desk[deskY][deskX];
+
+// длина маршрута всего ходов:
 int maxMove = deskX * deskY;
 
 // счётчик ходов:
 int stepCount;
-int result = 0;
 
 // описание хода коня в массиве из 8 векторов
 static int vektorXY[8][2] = {{1,  -2},
@@ -27,45 +36,56 @@ static int vektorXY[8][2] = {{1,  -2},
                              {-2, -1}
 };
 
-
 // рисуем доску
 void drawDesk() {
     int i, j;
     system("cls");
-    for (i = 0; i < 5; i++) {
-        for (j = 0; j < 5; j++) {
+    for (i = 0; i < deskY; i++) {
+        for (j = 0; j < deskX; j++) {
             printf("%4d", desk[i][j]);
         }
         puts("\n");
     }
-    printf("%d - move mumber.\n", stepCount);
+    printf("Move mumber %d\n", stepCount);
+    printf("Time: %ld millisecond\n", time_stop());
     //system("pause");
 }
 
-// поиск хода:
-// на вход подаются текущие координаты коня
+// поиск хода
+// на выходе функции будет 0, если в данную клетку нельзя встать конём,
+// на вход подаются текущие координаты коня:
 int getStep(int x, int y) {
-
-
-
-    int i;
-    if (x < 0 || x > 4 || y < 0 || y > 4)
-        return (0);
-    if (desk[x][y] != 0)
-        return (0);
-
-    //отметить свободное поле
-    desk[x][y] = ++stepCount;
-    drawDesk();
-
-    if (stepCount == 25)
-        return (1);
-    for (i = 0; i < 8; i++)
-        if (getStep(x + vektorXY[i][0], y + vektorXY[i][1]))
-            return 1;
-    stepCount--;
-    desk[x][y] = 0;
-    return 0;
+    int i = 0;
+    int result = 0;
+    // проверяем на выход за пределы поля:
+    if (x < 0 || x > 4 || y < 0 || y > 4) result = 0;
+    else {
+        // проверяем был ли конь тут раньше:
+        if (desk[x][y] != 0) result = 0;
+        else {
+            // считаем номер хода:
+            desk[x][y] = ++stepCount;
+            drawDesk();
+            // проверяем на конец маршрута:
+            if (stepCount == maxMove) result = 1;
+            else {
+                // перебираем возможные ходы из данной точки дальше
+                while (i < 8 & result != 1) {
+                    // рекурсивное ветвление с проверкой на возможность ходить дальше и на конец маршрута
+                    if (getStep(x + vektorXY[i][0], y + vektorXY[i][1])) result = 1;
+                    i++;
+                }
+                // если следующий ход невозможен,
+                if (result == 0) {
+                    // уменьшаем счётчик ходов
+                    stepCount--;
+                    // отходим с данной клетки
+                    desk[x][y] = 0;
+                }
+            }
+        }
+    }
+    return result;
 }
 
 // обнуляем доску и запускаем поиск хода
@@ -80,10 +100,26 @@ void path(int x0, int y0) {
 }
 
 int main() {
+    time_start();
     // стартуем из этой координаты, потому что
     // из неё точно есть решение:
     path(2, 2);
-    drawDesk();
     system("pause");
     return 0;
+}
+
+// Работа со временем
+void time_start() {
+    gettimeofday(&tv1, &tz);
+}
+
+long time_stop() {
+    gettimeofday(&tv2, &tz);
+    dtv.tv_sec = tv2.tv_sec - tv1.tv_sec;
+    dtv.tv_usec = tv2.tv_usec - tv1.tv_usec;
+    if (dtv.tv_usec < 0) {
+        dtv.tv_sec--;
+        dtv.tv_usec += 1000000;
+    }
+    return dtv.tv_sec * 1000 + dtv.tv_usec / 1000;
 }

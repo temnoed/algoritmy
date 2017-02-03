0fd27f116f484f66e12b52abe06eea9acfdb6927
temnoed <dc9257351633@gmail.com>
2017-02-03 05:49:42 +0300

    .

--- Lesson7/main.c
+++ Lesson7/main.c
@@ -1,10 +1,71 @@
+// Чистяков Дмитрий. ДЗ
 #include <stdio.h>
+
 //
+void solution1();
+
+//
+void solution2();
+
+//
+void solution3();
+
+//
+void solution4();
+
+void menu();
+
 int main() {
+    int sel;
+    do {
+        menu();
+        scanf("%i", &sel);
+        switch (sel) {
+            case 1:
+                solution1();
+                break;
+            case 2:
+                solution2();
+                break;
+            case 3:
+                solution3();
+                break;
+            case 4:
+                solution4();
+                break;
 
-
-
-
+            case 0:
+                printf("Bye-bye");
+                break;
+            default:
+                printf("Wrong selected\n");
+                break;
+        }
+    } while (sel != 0);
     return 0;
 }
 
+void solution1() {
+    return;
+}
+
+void solution2() {
+    return;
+}
+
+void solution3() {
+    return;
+}
+
+void solution4() {
+    return;
+}
+
+void menu() {
+    printf("\n1-task1 \n");
+    printf("2-task2 \n");
+    printf("3-task3 \n");
+    printf("4-task4 \n");
+
+    printf("0-exit\n");
+}
